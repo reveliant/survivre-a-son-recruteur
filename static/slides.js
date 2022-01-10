cee04ebe330efca267fb737b1dcfc59f8370fca7
Rémi Dubois <packman@oxiame.net>
2022-01-10 13:05:54 +0100

    Show notes by default

--- static/slides.js
+++ static/slides.js
@@ -1,10 +1,12 @@
 let revealHeader = document.querySelector('#reveal-header');
+let params = new URLSearchParams(document.location.search);
 Reveal.initialize({
     controls: true,
     progress: true,
     hash: true,
     disableLayout: true,
     plugins: [ RevealNotes ],
+    showNotes: ! params.has("hideNotes"),
 });
 Reveal.addEventListener('slidechanged', function( event ) {
     // event.previousSlide, event.currentSlide, event.indexh, event.indexv
